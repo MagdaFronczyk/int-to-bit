9c248a0fe6204484cb734a888156fe93afc6c31d
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-03-23 17:23:38 +0100

    divides into components and adds basic styling

---
+++ src/components/container/App.jsx
@@ -0,0 +1,52 @@
+import React, {
+  Component
+} from 'react';
+import './App.css';
+import Number from '../number/Number'
+
+class App extends Component {
+
+  state = {
+    number: '',
+    placeholder: 'enter your number'
+  }
+
+  validateNumber = input => {
+    const reg = /^\d+$/;
+    return reg.test(input);
+  };
+
+  checkIfString = input => {
+    const reg = /[^0-9]/;
+    return reg.test(input);
+  }
+
+  onInputChange = (event) => {
+    this.setState({
+      number: this.validateNumber(event.target.value) ? event.target.value : '',
+      placeholder: this.checkIfString(event.target.value) ? 'no letters allowed' : "enter your number"
+    })
+  }
+
+  render() {
+
+    const number = (
+      this.state.number !== "" && (this.state.number >>> 0).toString(2).split("").map((el, index) =>
+        < Number number={el} key={index} />)
+    )
+
+    return (
+      <div className='main-container'>
+        <label htmlFor='number' className='main-container_label'>Convert inetger to binary</label>
+        <input type="text" onChange={this.onInputChange} value={this.state.number} className='main-container_input'
+          placeholder={this.state.placeholder} id='number' />
+        <div className='main-container_number-wrapper'>
+          {number}
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default App;
---
+++ src/components/number/Number.jsx
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react';
+
+import './Number.css'
+
+class Number extends Component {
+
+  render() {
+    let color = "#" + ((1 << 24) * Math.random() | 0).toString(16);
+
+    return (
+      <p className='number-wrapper_number' style={{ color: `${color}` }}>{(this.props.number >>> 0).toString(2)}</p>
+    )
+  }
+}
+export default Number;
