605df8158a5be83734eb9da4b10eecc96df5af52
MagdaFronczyk <magdalenaoliwiaf@gmail.com>
2019-03-31 18:00:31 +0200

    chnages conditional rendering in NumberChanged component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -6,7 +6,7 @@ import './button.css'
 
 const Button = (props) => {
     return (
-        <button onClick={props.onClick} name={props.name} className={props.className}>{props.name}</button>
+        <button onClick={props.onClick} name={props.name} className={props.className} disabled={props.disabled}>{props.name}</button>
     )
 }
 
--- src/components/numberChanged/NumberChanged.jsx
+++ src/components/numberChanged/NumberChanged.jsx
@@ -38,40 +38,40 @@ class NumberChanged extends Component {
         return reg.test(input);
     }
 
+    setNumberAfterOperation = () => {
+        const { operator, operand } = this.state;
+        const { number } = this.props;
+
+        const OPERATIONS = {
+            ">>": <p className="number-changed_number">{(parseInt(number) >> parseInt(operand)).toString(2)}</p>,
+            ">>>": <p className="number-changed_number">{(parseInt(number) >>> parseInt(operand)).toString(2)}</p>,
+            "&": <p className="number-changed_number">{(parseInt(number) & parseInt(operand)).toString(2)}</p>,
+            "|": <p className="number-changed_number">{(parseInt(number) | parseInt(operand)).toString(2)}</p>,
+            "^": <p className="number-changed_number">{(parseInt(number) ^ parseInt(operand)).toString(2)}</p>,
+            "<<": <p className="number-changed_number">{(parseInt(number) << parseInt(operand)).toString(2)}</p>
+        }
+
+        return OPERATIONS[operator]
+    }
+
     render() {
         const { operator, placeholder, operand } = this.state;
         const { number } = this.props;
-
-        let numberAfterOperation;
-
-        if (operator === ">>") {
-            numberAfterOperation = (<p className="number-changed_number">{(parseInt(number) >> parseInt(operand)).toString(2)}</p>);
-        } else if (operator === ">>>") {
-            numberAfterOperation = (<p className="number-changed_number">{(parseInt(number) >>> parseInt(operand)).toString(2)}</p>);
-        } else if (operator === "&") {
-            numberAfterOperation = (<p className="number-changed_number">{(parseInt(number) & parseInt(operand)).toString(2)}</p>);
-        } else if (operator === "|") {
-            numberAfterOperation = (<p className="number-changed_number">{(parseInt(number) | parseInt(operand)).toString(2)}</p>);
-        } else if (operator === "^") {
-            numberAfterOperation = (<p className="number-changed_number">{(parseInt(number) ^ parseInt(operand)).toString(2)}</p>);
-        } else {
-            numberAfterOperation = (<p className="number-changed_number">{(parseInt(number) << parseInt(operand)).toString(2)}</p>)
-        }
 
         return (
             <div className="number-changed-container">
                 <label htmlFor='number' className='number-changed_label'>Choose your operator and operand</label>
                 <input type="text" onChange={this.onNumberInsert} name='operand' value={this.state.operand} placeholder={placeholder} className='number-changed_input' />
                 <div className="number-changed_button-container">
-                    <Button onClick={this.handleOperationChoice} name='>>' className='number-changed_button' />
-                    <Button onClick={this.handleOperationChoice} name='>>>' className='number-changed_button' />
-                    <Button onClick={this.handleOperationChoice} name='<<' className='number-changed_button' />
-                    <Button onClick={this.handleOperationChoice} name='&' className='number-changed_button' />
-                    <Button onClick={this.handleOperationChoice} name='|' className='number-changed_button' />
-                    <Button onClick={this.handleOperationChoice} name='^' className='number-changed_button' />
+                    <Button onClick={this.handleOperationChoice} name='>>' className='number-changed_button' disabled={!(operand !== "" && number !== "")} />
+                    <Button onClick={this.handleOperationChoice} name='>>>' className='number-changed_button' disabled={!(operand !== "" && number !== "")} />
+                    <Button onClick={this.handleOperationChoice} name='<<' className='number-changed_button' disabled={!(operand !== "" && number !== "")} />
+                    <Button onClick={this.handleOperationChoice} name='&' className='number-changed_button' disabled={!(operand !== "" && number !== "")} />
+                    <Button onClick={this.handleOperationChoice} name='|' className='number-changed_button' disabled={!(operand !== "" && number !== "")} />
+                    <Button onClick={this.handleOperationChoice} name='^' className='number-changed_button' disabled={!(operand !== "" && number !== "")} />
                 </div>
                 <div className="number-changed_number-container">
-                    {(operator !== "" && operand !== "" && number !== "") && numberAfterOperation}
+                    {(operator !== "" && operand !== "" && number !== "") && this.setNumberAfterOperation(operator)}
                 </div>
             </div>
         )
